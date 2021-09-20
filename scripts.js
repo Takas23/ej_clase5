b01ec85a60b91a996df30d3746784b03b4b2b47b
Takas23 <54868881+Takas23@users.noreply.github.com>
2021-09-20 03:21:39 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+var bLogin = document.querySelector("#login")
+var bSolicitudes = document.querySelector("#solicitudes")
+
+bLogin.addEventListener("click", function(){
+    location.href = "menu.html"
+})
+
+bSolicitudes.addEventListener("click", function(){
+    location.href = "solicitudes.html"
+})
